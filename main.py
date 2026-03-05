a975c541c60a6728e481d5e80844c75ac59efeb9
Cabeca568 <cristyanvictor568@gmail.com>
2026-03-05 18:39:38 -0300

    Dia 03

--- main.py
+++ main.py
@@ -1,24 +1,4 @@
-import sys
-
-def main():
-    # Definindo o valor de PI conforme o enunciado
-    pi = 3.14159
-    
-    try:
-        # Lendo a entrada do sistema (mais seguro para evitar Runtime Error)
-        entrada = sys.stdin.read().strip()
-        
-        if entrada:
-            raio = float(entrada)
-            
-            # Cálculo da área: A = π . raio²
-            area = pi * (raio ** 2)
-            
-            # Exibição com 4 casas decimais e fim de linha automático do print
-            print(f"A={area:.4f}")
-            
-    except EOFError:
-        pass
-
-if __name__ == "__main__":
-    main()
+raio = float(input())
+n = 3.14159
+area = n * (raio ** 2)
+print(f"A={area:.4f}")
